f8ef099e9d815a02239a6860b359cbb0c09fd551
Seydenu <128206248+Seydenu@users.noreply.github.com>
2023-06-01 19:03:37 +0000

    Add files via upload

---
+++ Assignment4.js
@@ -0,0 +1,4 @@
+let radius = 4;
+let area = Math.PI * radius ** 2;
+console.log(area); 
+Output: 50.26548245743669
---
+++ assignment.js
@@ -0,0 +1,5 @@
+let x = 10;
+let y = 3;
+let remainder = x % y;
+console.log(remainder);
+Output: 1
